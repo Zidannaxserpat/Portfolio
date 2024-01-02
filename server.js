88fb8ee7fab1fd55a5470e5c7ee534ad61dc378f
Zidannaxserpat <134835974+Zidannaxserpat@users.noreply.github.com>
2024-01-02 11:02:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,26 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 5001;
 
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
-});
+app.use(express.static('public'));
 
 app.post('/submit', (req, res) => {
-    const { Name, Email, Text } = req.body;
-  
-    // Format data
-    const formData = `Name: ${Name}\nEmail: ${Email}\nText: ${Text}\n\n`;
-  
-    // Tulis data ke file output.txt
-    fs.appendFile('output.txt', formData, (err) => {
-      if (err) throw err;
-      console.log('Data saved to output.txt');
-    });
-  
-    // Redirect to the home page
-    res.redirect('/'); 
+  const { Name, Email, Text } = req.body;
+
+  // Format data
+  const formData = `Name: ${Name}\nEmail: ${Email}\nText: ${Text}\n\n`;
+
+  // Tulis data ke file output.txt
+  fs.appendFile('output.txt', formData, (err) => {
+    if (err) throw err;
+    console.log('Data saved to output.txt');
+  });
+
+  res.send('Form submitted successfully!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
